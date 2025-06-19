8df47b9ab24968e7741a8583e6fb8ccdd2e82ca3
zeratheuz <zeratheuz@gmail.com>
2025-06-19 16:53:09 -0300

    Update HTML, CSS and JS

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 const myLibrary = []
 
+addBookToLibrary("All-Star Superman", "Grant Morrison", "Frank Quitely", "12", true);
+addBookToLibrary("V for Vendetta", "Alan Moore", "David Lloyd", "10", true);
+addBookToLibrary("Flash Forward", "Scott Lobdell", "Brett Booth", "6", false);
+addBookToLibrary("Batman: The Killing Joke", "Alan Moore", "Brian Bolland", "1", true);
+addBookToLibrary("Lobo: The Last Czarnian", "Keith Giffen & Alan Grant", "Simon Bisley", "4", false);
+addBookToLibrary("Green Lantern: Sinestro Corps War", "Geoff Johns & Dave Gibbons", "Ethan Van Sciver & Ivan Reis", "5", false);
+addBookToLibrary("Wonder Woman #1", "Tom King", "Daniel Sampere", "1", false);
+addBookToLibrary("Nightwing (2021)", "Tom Taylor", "Bruno Redondo", "30", false);
+addBookToLibrary("Future State: Superman/Wonder Woman", "Dan Watters", "Leila Del Duca & Lucas Meyer", "2", false);
+addBookToLibrary("Batman/Superman: World's Finest", "Mark Waid", "Dan Mora", "26", false);
+
+
 function Book(title, author, artist, issues, read) {
   this.title = title,
-  this.author = author,
-  this.artist = artist,
-  this.issues = issues,
-  this.read = read,
-  this.id = crypto.randomUUID()
+    this.author = author,
+    this.artist = artist,
+    this.issues = issues,
+    this.read = read,
+    this.id = crypto.randomUUID()
 }
 
 function addBookToLibrary(title, author, artist, issues, read) {
   myLibrary.push(new Book(title, author, artist, issues, read))
 }
+
+
+
+  const books = document.querySelector(".books")
+  books.textContent = myLibrary[-1]
+  for (let book in myLibrary) {
+
+    books.innerHTML += myLibrary[book].title
+  }
