c0451b04fffd57a669c319b0481a3346831b2de7
zeratheuz <zeratheuz@gmail.com>
2025-08-19 17:33:21 -0300

    Update JS

--- main.js
+++ main.js
@@ -1,5 +1,98 @@
 const myLibrary = []
 
+class Book {
+  constructor(title, writer, artist, issues, isRead) {
+    this.title = title,
+      this.writer = writer,
+      this.artist = artist,
+      this.issues = issues,
+      this.isRead = isRead,
+      this.id = crypto.randomUUID(),
+      this.isReadToggle = function () {
+        this.isRead === "Yes" ? this.isRead = "No" : this.isRead = "Yes"
+      }
+  }
+}
+
+function addBookToLibrary(object) {
+  myLibrary.push(new Book(object.title, object.writer, object.artist, object.issues, object.isRead))
+  displayBooks()
+  isRead()
+}
+
+function displayBooks() {
+  const container = document.querySelector(".container")
+  container.innerHTML = ''
+
+  myLibrary.forEach(book => {
+    const div = document.createElement("div")
+    div.setAttribute("class", "book")
+    div.setAttribute("id", book.id)
+
+    div.innerHTML = `
+    <button type="button" class="remove-button" data-bookid="${book.id}">x</button>
+    <div>
+      <p><b>Title: </b>${book.title}</p>
+      <p><b>Writer: </b>${book.writer}</p>
+      <p><b>Artist: </b>${book.artist}</p>
+      <p><b>Issues: </b>${book.issues}</p>
+      <p>
+      <b>Read: </b>
+      <button type="button" class="isRead-button" data-bookid="${book.id}">${book.isRead}</button>
+      </p>
+    <div/>
+      `
+
+    container.appendChild(div)
+
+    const removeButtons = document.querySelectorAll(".remove-button")
+    removeButtons.forEach(button => {
+
+      button.addEventListener("click", () => {
+        const id = button.dataset.bookid
+        const book = document.getElementById(id)
+
+        if (book) {
+          container.removeChild(book)
+
+          const index = myLibrary.findIndex(book => book.id === id)
+          myLibrary.splice(index, 1)
+        }
+      })
+    })
+  })
+}
+
+function isRead() {
+  const isReadButtons = document.querySelectorAll(".isRead-button")
+  isReadButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+      const id = button.dataset.bookid
+      const book = myLibrary.find(book => book.id == id)
+
+      book.isReadToggle()
+      button.textContent = book.isRead
+    })
+  })
+}
+
+const newBook = document.querySelector("#newBook")
+const closeDialog = document.querySelector("#closeDialog")
+const containerDialog = document.querySelector("#containerDialog")
+
+newBook.addEventListener("click", () => containerDialog.showModal())
+closeDialog.addEventListener("click", () => containerDialog.close())
+
+const form = document.querySelector("#bookData")
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  const bookData = new FormData(form)
+  const book = Object.fromEntries(bookData.entries())
+  addBookToLibrary(book)
+  containerDialog.close()
+})
+
 addBookToLibrary({
   title: "All-Star Superman",
   writer: "Grant Morrison",
@@ -95,94 +188,3 @@ addBookToLibrary({
   issues: "4",
   isRead: "No"
 });
-
-function Book(title, writer, artist, issues, isRead) {
-  this.title = title,
-    this.writer = writer,
-    this.artist = artist,
-    this.issues = issues,
-    this.isRead = isRead,
-    this.id = crypto.randomUUID(),
-    this.isReadToggle = function () {
-      this.isRead === "Yes" ? this.isRead = "No" : this.isRead = "Yes"
-    }
-}
-
-function addBookToLibrary(object) {
-  myLibrary.push(new Book(object.title, object.writer, object.artist, object.issues, object.isRead))
-  displayBooks()
-  isRead()
-}
-
-function displayBooks() {
-  const container = document.querySelector(".container")
-  container.innerHTML = ''
-
-  myLibrary.forEach(book => {
-    const div = document.createElement("div")
-    div.setAttribute("class", "book")
-    div.setAttribute("id", book.id)
-
-    div.innerHTML = `
-    <button type="button" class="remove-button" data-bookid="${book.id}">x</button>
-    <div>
-      <p><b>Title: </b>${book.title}</p>
-      <p><b>Writer: </b>${book.writer}</p>
-      <p><b>Artist: </b>${book.artist}</p>
-      <p><b>Issues: </b>${book.issues}</p>
-      <p>
-      <b>Read: </b>
-      <button type="button" class="isRead-button" data-bookid="${book.id}">${book.isRead}</button>
-      </p>
-    <div/>
-      `
-
-    container.appendChild(div)
-
-    const removeButtons = document.querySelectorAll(".remove-button")
-    removeButtons.forEach(button => {
-
-      button.addEventListener("click", () => {
-        const id = button.dataset.bookid
-        const book = document.getElementById(id)
-
-        if (book) {
-          container.removeChild(book)
-
-          const index = myLibrary.findIndex(book => book.id === id)
-          myLibrary.splice(index, 1)
-        }
-      })
-    })
-  })
-}
-
-function isRead() {
-  const isReadButtons = document.querySelectorAll(".isRead-button")
-  isReadButtons.forEach(button => {
-
-    button.addEventListener("click", () => {
-      const id = button.dataset.bookid
-      const book = myLibrary.find(book => book.id == id)
-
-      book.isReadToggle()
-      button.textContent = book.isRead
-    })
-  })
-}
-
-const newBook = document.querySelector("#newBook")
-const closeDialog = document.querySelector("#closeDialog")
-const containerDialog = document.querySelector("#containerDialog")
-
-newBook.addEventListener("click", () => containerDialog.showModal())
-closeDialog.addEventListener("click", () => containerDialog.close())
-
-const form = document.querySelector("#bookData")
-form.addEventListener("submit", (event) => {
-  event.preventDefault()
-  const bookData = new FormData(form)
-  const book = Object.fromEntries(bookData.entries())
-  addBookToLibrary(book)
-  containerDialog.close()
-})
